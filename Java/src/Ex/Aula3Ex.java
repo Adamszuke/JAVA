1ae4213a232a4e5e4375ed1fb7e550c7aab7eebd
Adamszuke <g.nakagawazuke@gmail.com>
2025-06-12 20:56:32 -0300

    Ex3 Atv2

--- Java/src/Ex/Aula3Ex.java
+++ Java/src/Ex/Aula3Ex.java
@@ -2,28 +2,36 @@ package Ex;
 import java.util.Scanner;
 
 public class Aula3Ex {
+    static double n1; 
+    static double n2;
+    static double n3;
+    static double media;
+    static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        
+        
+        
+        
+        
         System.out.println("Qual atividade você deseja acessar? ");
         int atv = scanner.nextInt();
-
+        
         switch (atv) {
             case 1:
                 Atv1();
                 break;
-        
+            case 2:
+                Atv2();
             default:
                 break;
         }
-        
+
         
 
     }
 
     static void Atv1()
     {
-        Scanner scanner = new Scanner(System.in);
-        
         // Desenvolva um sistema básico de login que utilize procedimentos para exibir mensagens e funções para validar credenciais.
 
         String user;
@@ -45,5 +53,25 @@ public class Aula3Ex {
         
     }
 
+    static void Atv2(){
+        // Desenvolva um sistema para gerenciar notas de estudantes, utilizando subrotinas para entrada, processamento e exibição de dados
 
+        System.out.println("Digite a primeira nota");
+        n1 = scanner.nextDouble();
+        System.out.println("Digite a segunda nota");
+        n2 = scanner.nextDouble();
+        System.out.println("Digite a terceira nota");
+        n3 = scanner.nextDouble();
+
+        media();
+        System.out.printf("Sua média final é: %s",media);
+
+
+
+
+    }
+
+    static void media(){
+        media = (n1 + n2 + n3) / 3;
+    }
 }
